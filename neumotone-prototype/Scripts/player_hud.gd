8622d4c581fdd892b39c7fac6345af8b22a99dff
XBotDoesStuff <Xanderchuck@gmail.com>
2026-01-15 09:50:38 -0700

    started crosshair
    still need to make it change when looking at interactable

---
+++ neumotone-prototype/Scripts/player_hud.gd
@@ -0,0 +1,15 @@
+extends Control
+
+@onready var crosshair: TextureRect = $Crosshair
+
+const NON_INTERACT_CROSSHAIR = preload("uid://ydhjwtify55i")
+const INTERACT_CROSSHAIR = preload("uid://edbd6v152kbm")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	crosshair.texture = NON_INTERACT_CROSSHAIR
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
